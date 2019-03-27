5536512e36ed59d1f97b938f1e948b2e9123d9c4
Olushi Luqman <olushilukmon03@gmail.com>
2019-03-27 07:12:25 +0100

    refactor(bundle): include sourcemaps in bundle

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript';
 import pkg from './package.json';
 
-const sourcemap = false;
+const sourcemap = true;
 
 export default {
   input: 'lib/index.ts',
