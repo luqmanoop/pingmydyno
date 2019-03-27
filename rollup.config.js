97c948299a288808b6781aad717a5e38e3252e9f
Olushi Luqman <olushilukmon03@gmail.com>
2019-03-27 04:31:11 +0100

    chore(bundle): add rollup
    add a module bundler (rollup) for publishing new releases

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import babel from 'rollup-plugin-babel';
+// import { eslint } from 'rollup-plugin-eslint';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript';
+import pkg from './package.json';
+
+const sourcemap = false;
+
+export default {
+  input: 'lib/index.ts',
+  external: ['is-reachable'],
+  plugins: [
+    typescript(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    terser(),
+  ],
+  output: [
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap,
+    },
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap,
+    },
+  ],
+};
